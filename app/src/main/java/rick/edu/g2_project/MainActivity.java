f45b114b6547978c5e9ccf470e2f3b9f8318a6a5
Rick Dymond <rdymond@oakland.edu>
2022-11-20 22:19:48 -0500

    added first view logic

--- app/src/main/java/rick/edu/g2_project/MainActivity.java
+++ app/src/main/java/rick/edu/g2_project/MainActivity.java
@@ -1,13 +1,37 @@
 package rick.edu.g2_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView homeTitle;
+    Button homeBTN;
+    Intent mainPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        homeTitle = findViewById(R.id.homeTitle);
+        homeBTN = findViewById(R.id.homeBTN);
+
+        mainPage = new Intent(this, exploreActivity.class);
+
+        homeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                MainActivity.this.startActivity(mainPage);
+            }
+        });
+
+
+
+
     }
 }
---
+++ app/src/main/java/rick/edu/g2_project/exploreActivity.java
@@ -0,0 +1,13 @@
+package rick.edu.g2_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class exploreActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_explore);
+    }
+}
