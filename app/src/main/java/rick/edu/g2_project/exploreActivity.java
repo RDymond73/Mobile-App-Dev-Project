d663fdfd2a3787f1ff797e07265b04771596d69f
Rick Dymond <rdymond@oakland.edu>
2022-11-28 02:25:47 -0500

    working on third layout

--- app/src/main/java/rick/edu/g2_project/exploreActivity.java
+++ app/src/main/java/rick/edu/g2_project/exploreActivity.java
@@ -1,5 +1,6 @@
 package rick.edu.g2_project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -25,6 +26,13 @@ public class exploreActivity extends AppCompatActivity {
     ArrayAdapter<String> SpinnerAdapter;
     String[] filters = {"All", "CPUs", "GPUs"};
 
+    String[] partDescs = {"This processor features a performance hybrid architecture designed for intelligent performance, optimized creating, and enhanced tuning to allow gamers to game with up to 5.8 GHz clock speed.", "test"};
+
+    String[] partLinks = {"Amazon, Intel, Best Buy", "test"};
+
+    Intent intentPart;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +48,84 @@ public class exploreActivity extends AppCompatActivity {
         SpinnerAdapter = new ArrayAdapter<String>(this, R.layout.spinner_item, filters);
         SpinnerFilter.setAdapter(SpinnerAdapter);
 
+        intentPart = new Intent(this, item_activity.class);
+
+
+        //list on item click
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String position = listView.getItemAtPosition(i).toString();
+                if (i == 0) {
+//////                    String partName = partNames[i];
+//////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////////                    intentPart.putExtra("partName", partName);
+////////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+                    exploreActivity.this.startActivity(intentPart);
+//                exploreActivity.this.startActivity(intentPart);
+                }
+//                else if (i == 1) {
+////                    String partName = partNames[i];
+////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////                    intentPart.putExtra("partName", partName);
+////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+//                    exploreActivity.this.startActivity(intentPart);
+//                }
+//                else if (i == 2) {
+////                    String partName = partNames[i];
+////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////                    intentPart.putExtra("partName", partName);
+////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+//                    exploreActivity.this.startActivity(intentPart);
+//                }
+//                else if (i == 3) {
+////                    String partName = partNames[i];
+////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////                    intentPart.putExtra("partName", partName);
+////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+//                    exploreActivity.this.startActivity(intentPart);
+//                }
+//                else if (i == 4) {
+////                    String partName = partNames[i];
+////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////                    intentPart.putExtra("partName", partName);
+////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+//                    exploreActivity.this.startActivity(intentPart);
+//                }
+//                else if (i == 5) {
+////                    String partName = partNames[i];
+////                    String partPrice = partPrices[i];
+//                    String partDesc = partDescs[i];
+//                    String partLink = partLinks[i];
+////                    intentPart.putExtra("partName", partName);
+////                    intentPart.putExtra("partPrice", partPrice);
+//                    intentPart.putExtra("partDesc", partDesc);
+//                    intentPart.putExtra("partLink", partLink);
+//                    exploreActivity.this.startActivity(intentPart);
+//                }
+            }
+        });
+
     }
 
     private void loadData() {
@@ -48,4 +134,5 @@ public class exploreActivity extends AppCompatActivity {
         myAdapter = new myAdapter(this, myList);
         listView.setAdapter(myAdapter);
     }
+
 }
